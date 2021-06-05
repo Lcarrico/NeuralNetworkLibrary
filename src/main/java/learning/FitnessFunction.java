077454507cdec5e62abccf7b2a30a543b903e15f
Leo Carrico <lcarrico827@gmail.com>
2021-06-05 14:46:06 -0500

    learning package documentation

--- src/main/java/learning/FitnessFunction.java
+++ src/main/java/learning/FitnessFunction.java
@@ -4,6 +4,10 @@ import base.NeuralNetwork;
 
 import java.util.ArrayList;
 
+/**
+ * This interface will be the lambda function for the fitness score of the genetic algorithm.
+ * @see GeneticAlgorithm
+ */
 @FunctionalInterface
 public interface FitnessFunction{
      Float calculate(NeuralNetwork<ArrayList<Integer>> nn);
